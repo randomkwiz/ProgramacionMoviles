9aa37a603516f5f800708b6a687fc044aff5acfc
Angela <angyvazquezdel97@gmail.com>
2019-09-23 10:20:35 +0200

    Proyecto Hola Mundo

---
+++ HolaMundo/app/src/main/java/es/iesnervion/avazquez/holaMundo/MainActivity.java
@@ -0,0 +1,20 @@
+package es.iesnervion.avazquez.holaMundo;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+import android.view.View;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main); //le dice que xml es el fichero de layaout de esta actividad
+
+        /*la carpeta res es de resources, ahi van las imagenes, etc
+        en res/mipmap va el icono de la app y eso
+        * */
+    }
+
+}
