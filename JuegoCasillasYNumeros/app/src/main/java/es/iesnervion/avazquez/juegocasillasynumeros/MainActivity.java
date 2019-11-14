f0449ccaa7bafe2d571095697389bc725fac80d0
Angela <angyvazquezdel97@gmail.com>
2019-11-14 12:45:48 +0100

    juego

---
+++ JuegoCasillasYNumeros/app/src/main/java/es/iesnervion/avazquez/juegocasillasynumeros/MainActivity.java
@@ -0,0 +1,21 @@
+package es.iesnervion.avazquez.juegocasillasynumeros;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+
+
+        /*
+        * INFO AQUI SOBRE CREAR TABLERO EN CONSTRAINT LAYOUT
+        * https://stackoverflow.com/questions/51430129/create-grid-n-%C3%97-n-in-android-constraintlayout-with-variable-number-of-n
+        * */
+    }
+}
