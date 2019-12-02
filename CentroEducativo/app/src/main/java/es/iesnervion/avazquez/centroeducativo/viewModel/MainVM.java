5868d1a59b068013e61d1210f63f90e30b899a3d
Angela <angyvazquezdel97@gmail.com>
2019-12-02 09:05:03 +0100

    he cambiado los tamaños

--- CentroEducativo/app/src/main/java/es/iesnervion/avazquez/centroeducativo/viewModel/MainVM.java
+++ CentroEducativo/app/src/main/java/es/iesnervion/avazquez/centroeducativo/viewModel/MainVM.java
@@ -31,34 +31,38 @@ public class MainVM extends ViewModel {
 
     private void cargarPersonas() {
         ArrayList<Persona> personaArrayList = new ArrayList<>();
-
+        personaArrayList.add(new Profesor("Asuncion", "Criado", R.drawable.professorfemale, "Programacion"));
         personaArrayList.add(new Profesor("Fernando", "Galiana", R.drawable.professor, ".NET"));
         personaArrayList.add(new Profesor("Miguel Angel", "Casado", R.drawable.professor, "Android"));
-        personaArrayList.add(new Alumno("Ángela"));
-        personaArrayList.add(new Alumno("Iván"));
-        personaArrayList.add(new Profesor("Asuncion", "Criado", R.drawable.professor, "Programacion"));
-        personaArrayList.add(new Alumno("Nzhdeh"));
-        personaArrayList.add(new Alumno("Dani"));
         personaArrayList.add(new Profesor("Leo", "Acal", R.drawable.professor, "BBDD"));
-        personaArrayList.add(new Alumno("Gonzalo"));
-        personaArrayList.add(new Alumno("Pablo"));
         personaArrayList.add(new Profesor("Antonio", "", R.drawable.professor, "Acceso a Datos"));
         personaArrayList.add(new Profesor("Vicky", "", R.drawable.professor, "Inglés"));
 
+        personaArrayList.add(new Alumno("Ángela"));
+        personaArrayList.add(new Alumno("Iván"));
+        personaArrayList.add(new Alumno("Nzhdeh"));
+        personaArrayList.add(new Alumno("Dani"));
+        personaArrayList.add(new Alumno("Gonzalo"));
+        personaArrayList.add(new Alumno("Pablo"));
+
         Profesor profesor = new Profesor("Profesor", "Profesor", R.drawable.professor, "Asignatura");
+        Profesor profesorfemale = new Profesor("Profesora", "Profesora", R.drawable.professorfemale, "Asignatura");
+
         Alumno alumno = new Alumno(("Alumno"));
 
         //Aleatorio para añadir profesores y alumnos
         for(int i = 0; i < 1000; i ++){
-            int max = 10;
+            int max = 9;
             int min = 1;
             int range = (max - min) + 1;
             int random =  (int)(Math.random() * range) + min;
 
-            if(random > 5){
+            if(random <= 3){
                 personaArrayList.add(profesor);
-            }else{
+            }else if (random <= 6){
                 personaArrayList.add(alumno);
+            }else{
+                personaArrayList.add(profesorfemale);
             }
         }
 
